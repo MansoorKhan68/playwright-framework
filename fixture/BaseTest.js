8e51223d679ea568729e22c60154af23386e0262
Mansoor Khan <mansoorkhanlkl99@gmail.com>
2026-02-10 00:40:21 +0500

    feat: Add UserPage class and integrate it into BaseTest for user management

--- fixture/BaseTest.js
+++ fixture/BaseTest.js
@@ -1,6 +1,7 @@
 import { test as baseTest } from '@playwright/test';
 import LoginPage from '../pages/LoginPage';
 import CommonUtils from '../utils/CommonUtils';
+import UserPage from '../pages/UserPage';
 
 export const test = baseTest.extend({
   loginPage: async ({ page }, use) => {
@@ -11,6 +12,10 @@ export const test = baseTest.extend({
     const commonUtils = new CommonUtils();
     await use(commonUtils);
   },
+  userPage: async ({ page }, use) => {
+    const userPage = new UserPage(page);
+    await use(userPage);
+  }
 });
 
 export { expect } from '@playwright/test';
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,11 @@
 import { test, expect } from '../fixture/BaseTest';
 
+// use fixtures to perform login before each test and logout after each test
+
+// test.afterEach(async ({ userPage }) => {
+//     await userPage.logout();
+//     console.log('Logged out after test');
+// });
 test('Login Test', async ({ page, loginPage, commonUtils }) => {
     await loginPage.gotoLoginPage();
    console.log('Login successful with global setup, skipping login steps in this test');
